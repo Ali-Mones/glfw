aa5c80030741ba2c3c52a210001b1099f23f29f7
Ali Mo'nes <AliIsHomie@outlook.com>
2023-09-17 14:17:06 +0300

    added premake

---
+++ premake5.lua
@@ -0,0 +1,80 @@
+project "glfw"
+    kind "StaticLib"
+    language "C"
+    staticruntime "off"
+
+    files {
+        "src/internal.h",
+        "src/platform.h",
+        "src/mappings.h",
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/platform.c",
+        "src/vulkan.c",
+        "src/window.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c",
+        "src/null_platform.h",
+        "src/null_joystick.h",
+        "src/null_init.c",
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/null_joystick.c",
+    }
+
+    targetdir "%{wks.location}/bin/%{cfg.buildcfg}/%{prj.name}"
+    objdir "%{wks.location}/bin-int/%{cfg.buildcfg}/%{prj.name}"
+
+    includedirs { 
+        "%{prj.location}/src",
+        "%{prj.location}/vendor/GLFW/include"
+    }
+
+    filter "system:windows"
+        files {
+            "src/win32_time.h",
+            "src/win32_thread.h",
+            "src/win32_module.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_platform.h",
+            "src/win32_joystick.h",
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+        }
+        defines {
+            "_GLFW_WIN32"
+        }
+
+    filter "system:linux"
+        files {
+            "src/posix_time.h",
+            "src/posix_thread.h",
+            "src/posix_module.c",
+            "src/posix_time.c",
+            "src/posix_thread.c",
+            "src/x11_platform.h",
+            "src/xkb_unicode.h",
+            "src/x11_init.c",
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+            "src/xkb_unicode.c",
+            "src/glx_context.c",
+        }
+        defines {
+			"_GLFW_X11"
+		}
+        pic "On"
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
